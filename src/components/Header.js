3dcb38d593fd18108b07811f77926648fd885686
paulrcook <paul@lougeek.com>
2017-10-18 16:07:14 -0400

    wired up route to login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,30 @@
-'use strict';
-
 import React from 'react';
+import {Link} from 'react-router';
 
 class Header extends React.Component {
     render() {
         return (
-          <nav className="navbar navbar-light">
-              <div className="container">
-                  <a className="navbar-brand">
-                      {this.props.appName.toLowerCase()}
-                  </a>
-              </div>
-          </nav>
+            <nav className="navbar navbar-light">
+                <div className="container">
+                    <Link to='/' className="navbar-brand">
+                        {this.props.appName.toLowerCase()}
+                    </Link>
+
+                    <ul className="nav navbar-nav pull-xs-right">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                Home
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to="login" className="nav-link">
+                                Sign in
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
